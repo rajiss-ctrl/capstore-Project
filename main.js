e3b225ce88036970833866944e66680be8b459a9
Raji Sarafadeen Sanjo <omosanjos77@gmail.com>
2020-11-19 21:02:06 -0800

    capstor project

---
+++ main.js
@@ -0,0 +1,29 @@
+let carts = document.querySelectorAll(".add-cart");
+
+for(let i =0;i < carts.length; i++){
+    carts[i].addEventListener('click',()=>{
+        cartNumbers();
+    });
+}
+function onLoadCartNumbers(){
+    let productNumbers = localStorage.getItem('cartNumbers');
+    if(productNumbers){
+        document.querySelector('.cart span').textContent = productNumbers;
+    }  
+}
+
+function cartNumbers(){
+    let productNumbers = localStorage.getItem('cartNumbers');
+    
+    productNumbers=parseInt(productNumbers);
+    if(productNumbers){
+    
+    localStorage.setItem('cartNumbers',productNumbers + 1);
+    document.querySelector('.cart span').textContent = productNumbers +1;
+    }else{
+        localStorage.setItem('cartNumbers', 1);
+        document.querySelector('.cart span').textContent = 1;
+    }
+    
+}
+onLoadCartNumbers();
