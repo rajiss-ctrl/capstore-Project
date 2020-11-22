a7640c84c4e991e413e936aabf6ba30ac9b868eb
Raji Sarafadeen Sanjo <omosanjos77@gmail.com>
2020-11-22 01:42:14 -0800

    capstore work in progress

--- main.js
+++ main.js
@@ -1,8 +1,128 @@
+
+
 let carts = document.querySelectorAll(".add-cart");
+
+let products = [
+    {
+        name:'brownBrim',
+        tag:'brim',
+        price:'25',
+        inCart:'0'
+    },
+    {
+        name:'blueBeanie',
+        tag:'beanie',
+        price:'18',
+        inCart:'0'
+    },
+    {
+        name:'brownCowBoy',
+        tag:'cowboy',
+        price:'35',
+        inCart:'0'
+    },
+    {
+        name:'greyBrim',
+        tag:'greyb',
+        price:'25',
+        inCart:'0'
+    },
+    {
+        name:'addidas',
+        tag:'nmd',
+        price:'220',
+        inCart:'0'
+    },
+    {
+        name:'addidasYeezy',
+        tag:'yeezy',
+        price:'280',
+        inCart:'0'
+    },
+    {
+        name:'blackConverse',
+        tag:'converse',
+        price:'110',
+        inCart:'0'
+    },
+    {
+        name:'nikeWhite',
+        tag:'nike',
+        price:'160',
+        inCart:'0'
+    },
+    {
+        name:'blackJean',
+        tag:'jean',
+        price:'125',
+        inCart:'0'
+    },
+    {
+        name:'blueJeanJacket',
+        tag:'jacket',
+        price:'90',
+        inCart:'0'
+    },
+    {
+        name:'brownShearling',
+        tag:'bshearl',
+        price:'165',
+        inCart:'0'
+    },
+    {
+        name:'blueTanktop',
+        tag:'tanktop',
+        price:'25',
+        inCart:'0'
+    },
+    {
+        name:'florablouse',
+        tag:'blousef',
+        price:'20',
+        inCart:'0'
+    },
+    {
+        name:'floradress',
+        tag:'florad',
+        price:'80',
+        inCart:'0'
+    },
+    {
+        name:'reddotdress',
+        tag:'reddot',
+        price:'80',
+        inCart:'0'
+    },
+    {
+        name:'camodownvest',
+        tag:'camo',
+        price:'325',
+        inCart:'0'
+    },
+    {
+        name:'floratshirt',
+        tag:'tshirt',
+        price:'20',
+        inCart:'0'
+    },
+    {
+        name:'blackwhitelong',
+        tag:'sleeve',
+        price:'25',
+        inCart:'0'
+    },
+    {
+        name:'pinktshirt',
+        tag:'pink',
+        price:'25',
+        inCart:'0'
+    }
+]
 
 for(let i =0;i < carts.length; i++){
     carts[i].addEventListener('click',()=>{
-        cartNumbers();
+        cartNumbers(products[i]);
+        totalCost(products[i]);
     });
 }
 function onLoadCartNumbers(){
@@ -12,7 +132,8 @@ function onLoadCartNumbers(){
     }  
 }
 
-function cartNumbers(){
+function cartNumbers(product){
+    console.log("The product click", product);
     let productNumbers = localStorage.getItem('cartNumbers');
     
     productNumbers=parseInt(productNumbers);
@@ -24,6 +145,38 @@ function cartNumbers(){
         localStorage.setItem('cartNumbers', 1);
         document.querySelector('.cart span').textContent = 1;
     }
-    
+    setItems(product);
+}
+function setItems(product){
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
+    if(cartItems != null){
+        if(cartItems[product.tag] == undefined){
+            cartItems={
+                ...cartItems,[product.tag]:product
+            }
+        }
+        cartItems[product.tag].inCart += 1;
+    }else{
+        product.inCart = 1;
+        cartItems = {
+           [product.tag]:product
+    }
+}
+localStorage.setItem('productsInCart', JSON.stringify(cartItems));
+
+}
+function totalCost(product){
+// console.log("the product price is " , product.price);
+let cartCost = localStorage.getItem('totalCost');
+
+if(cartCost != null){
+    cartCost = parseInt(cartCost);
+    localStorage.setItem("totalCost", cartCost + product.price);
+    console.log(product.price + cartCost);
+}else{
+    localStorage.setItem("totalCost", product.price);
+}
+
 }
 onLoadCartNumbers();
